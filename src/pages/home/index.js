9dc7e67178c78ce9f3fffd300fad23349672c85a
warnal-labs <warnallabs@gmail.com>
2024-06-22 13:26:14 +0530

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -41,7 +41,7 @@ export const Home = () => {
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
                     <div id="button_p" className="ac_btn btn ">
-                      My Portfolio
+                      Works of Warnal Labs 
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
